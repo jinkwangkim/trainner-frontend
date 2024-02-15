ce2ab38036f06ab7ec2f56b60656c7b788ebb1e4
kwang23 <kwang23@gmail.com>
2024-02-15 14:11:35 +0900

    프로젝트 초기화

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+//module.exports = defineConfig({
+//  transpileDependencies: true
+//})
+
+module.exports = {
+  outputDir: "../src/main/resources/static",  // 빌드 타겟 디렉토리
+  devServer: {
+    proxy: {
+      '/api': {
+        // '/api' 로 들어오면 포트 8090(스프링 서버)로 보낸다
+        target: 'http://localhost:8090',
+        changeOrigin: true // cross origin 허용
+      }
+    }
+  }
+};
